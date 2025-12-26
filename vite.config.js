0c6a36478d87e9f9d2d3bbadb2a6e1404335bf11
nechostel21k4 <nechostel21k4@gmail.com>
2025-12-26 10:38:57 +0530

    app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Hostel Student App',
+        short_name: 'StudentApp',
+        description: 'Student Hostel Management App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'Nec.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'Nec.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
